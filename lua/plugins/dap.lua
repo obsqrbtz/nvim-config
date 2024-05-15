38e5df91ef47a78d4b58a1037dbe4ac9a3f2ef5f
Daniel Dada <doesdeos@gmail.com>
2024-05-15 20:42:08 +0300

    updated dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -4,7 +4,7 @@ return{
 		local dap = require("dap")
 		dap.adapters.coreclr = {
 			type = "executable",
-			command = "netcoredbg",
+			command = "/usr/local/netcoredbg",
 			args = {"--interpreter=vscode"}
 		}
 		dap.configurations.cs = {
