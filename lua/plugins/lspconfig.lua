835e9cd0fbc0e3bd8cd27e4255e92b60ab4a75bb
Daniel Dada <doesdeos@gmail.com>
2024-05-19 17:49:11 +0300

    hyprlang LSP

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -110,6 +110,23 @@ return {
 				prefix = "",
 			},
 		}
+		-- Hyprlang LSP
+		local home = os.getenv( "HOME" )
+		vim.api.nvim_create_autocmd({'BufEnter', 'BufWinEnter'}, {
+			pattern = {"*.hl", "hypr*.conf"},
+			callback = function(event)
+				print(string.format("starting hyprls for %s", vim.inspect(event)))
+				vim.lsp.start {
+						name = "hyprlang",
+						autostart = true,
+						cmd = {home .. "/go/bin/hyprls"},
+						root_dir = vim.fn.getcwd(),
+				}
+			end
+		})
+		vim.filetype.add({
+  		pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
+		})
 		vim.diagnostic.config(config)
 	end,
 }
